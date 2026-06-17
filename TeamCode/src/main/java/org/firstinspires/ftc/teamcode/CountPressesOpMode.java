b1507a8d12a885f09764414d277018ed5e34bb86
hayes <hayesmeier@gmail.com>
2026-06-16 19:50:24 -0500

    Fixed touch sensor to count by one by each press ;) (also played with a mototr with jacob) ;)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CountPressesOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CountPressesOpMode.java
@@ -13,14 +13,16 @@ public class CountPressesOpMode extends LinearOpMode {
         touchSensor = hardwareMap.get(TouchSensor.class, "testTouch");
 
         waitForStart();
+        int pressCount = 0; // Our counter variable
+        boolean lastpressed = false;
 
         while (opModeIsActive()) {
-            int pressCount = 0; // Our counter variable
+           
 
-            if (touchSensor.isPressed()) {
+            if (!lastpressed && touchSensor.isPressed()) {
                 pressCount = pressCount + 1;
             }
-
+            lastpressed = touchSensor.isPressed();
             telemetry.addData("Sensor Press Count", pressCount);
             telemetry.update();
         }
